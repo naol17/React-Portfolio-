a21e3abbe38de01501f519932118c9b8398e58f9
Naol Dame <naododa5566@gmail.com>
2022-05-20 21:06:10 +0300

    Creating Intro Section

---
+++ my-portfolio/src/Components/Intro/intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Intro.css'
+
+const Intro =()=>{
+    return(
+        <div className='intro'>
+            <div className='intro-left'>
+                <div className='intro-name'>
+                    <span>Hey! I Am</span>
+                    <span>Naol Dame</span>
+                    <span>Frontend Developer and graphic designer with high creative and 
+                            quality development, producing quality work.
+                    </span>
+
+                </div>
+            </div>
+            <div className='button intro-btn'>Hire me</div>
+            <div className='intro-right'>right side</div>
+        </div>
+    )
+}
+
+export default Intro;
