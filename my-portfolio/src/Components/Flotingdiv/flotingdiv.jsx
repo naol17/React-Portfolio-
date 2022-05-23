254ed93bc14857c23da8a07de0c18e2bef99c310
Naol Dame <naododa5566@gmail.com>
2022-05-23 09:45:26 +0300

    update intro section

---
+++ my-portfolio/src/Components/Flotingdiv/flotingdiv.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Floatingdiv = () =>{
+    return(
+        <div className='floatingdiv'>
+            <span>Web
+                <br/>
+                Developer
+            </span>
+
+        </div>
+    )
+}
+
+export default Floatingdiv;
--- my-portfolio/src/Components/Intro/intro.jsx
+++ my-portfolio/src/Components/Intro/intro.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import './Intro.css'
-import Github from '../../assets/img/github.png'
-import Insta from '../../assets/img/instagram.png'
-import linked from '../../assets/img/linkedin.png'
-
-const Intro =()=>{
-    return(
-        <div className='intro'>
-            <div className='intro-left'>
-                <div className='intro-name'>
-                    <span>Hey! I Am</span>
-                    <span>Naol Dame</span>
-                    <span>Frontend Developer and graphic designer with high creative and 
-                            quality development, producing quality work.
-                    </span>
-
-                </div>
-<button className='button intro-btn'>Hire Me</button>
-            </div>
-            <div>
-                
-            </div>
-            <div className='intro-right'>right side</div>
+import React from "react";
+import "./Intro.css";
+import Github from "../../assets/img/github.png";
+import Insta from "../../assets/img/instagram.png";
+import linked from "../../assets/img/linkedin.png";
+import Vector1 from "../../assets/img/Vector1.png";
+import Vector2 from "../../assets/img/Vector2.png";
+import boy from "../../assets/img/boy-r.png";
+import crowun from "../../assets/img/crown.png";
+import thumbup from "../../assets/img/thumbup.png";
+import floatingdiv from "../Flotingdiv/flotingdiv";
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="intro-left">
+        <div className="intro-name">
+          <span>Hey! I Am</span>
+          <span>Naol Dame</span>
+          <span>
+            Frontend Developer and graphic designer with high creative and
+            quality development, producing quality work.
+          </span>
         </div>
-    )
-}
+        <button className="button intro-btn">Hire Me</button>
+        <div className="intro-icon">
+          <img src={Github}></img>
+          <img src={Insta}></img>
+          <img src={linked}></img>
+        </div>
+      </div>
+
+      <div className="intro-right">
+        <img src={Vector1} />
+        <img src={Vector2} />
+        <img src={boy} />
+
+        <div>
+          <floatingdiv />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
