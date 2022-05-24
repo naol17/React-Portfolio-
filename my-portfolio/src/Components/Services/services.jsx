1311adeabd605828ac39da1ba503205364c37c92
Naol Dame <naododa5566@gmail.com>
2022-05-25 00:19:32 +0300

    Creating Service Section

---
+++ my-portfolio/src/Components/Services/services.jsx
@@ -0,0 +1,14 @@
+import  React  from "react";
+
+const Services = ()=>{
+    return(
+        <div className="services">
+
+            <div className="awesome"></div>
+            <div className="cards"></div>
+
+        </div>
+    )
+
+}
+export default Services;
